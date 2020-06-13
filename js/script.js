adfd856557ada02cacadefc4a45cd599740163b6
Futo Nakajima <maple0922wh-142857@i.Softbank.jp>
2020-06-13 16:41:19 +0900

    2020/06/13 16:41

--- js/script.js
+++ js/script.js
@@ -1,3 +1,31 @@
-$(function(){
-
+const app = new Vue({
+  el: '.pages',
+  data: {
+    items: [
+      {
+        name: 'Gamble Calculator',
+        url: 'gamble-calculator'
+      },
+      {
+        name: 'Image PDF Converter',
+        url: 'image-pdf-converter'
+      },
+      {
+        name: 'test',
+        url: 'image-pdf-converte'
+      },
+      {
+        name: 'test',
+        url: 'image-pdf-conrter'
+      },
+      {
+        name: 'test',
+        url: 'image-pdf-converer'
+      },
+      {
+        name: 'test',
+        url: 'image-pdf-coter'
+      }
+    ]
+  }
 });
